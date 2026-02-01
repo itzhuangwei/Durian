5094a8c5055410ebb0149338b20a57c5097f2b20
zhuangwei <42955983+itzhuangwei@users.noreply.github.com>
2026-02-01 22:47:01 +0800

    add: rabbitmq - work queue

--- durian-rabbitmq/consumer-service/src/main/java/com/zwx/durian/mq/consumer/listener/MyListener.java
+++ durian-rabbitmq/consumer-service/src/main/java/com/zwx/durian/mq/consumer/listener/MyListener.java
@@ -8,8 +8,30 @@ public class MyListener {
 
     private static final String QUEUE_NAME = "test-simple-queue";
 
+    private static final String WORK_QUEUE_NAME = "test.work.queue";
+
+    /**
+     * 监听队列 - 简单消息队列模式
+     *
+     * @param message
+     */
     @RabbitListener(queues = QUEUE_NAME)
     public void onMessage(String message) {
         System.out.println("接收到消息：" + message);
+    }
+
+    /**
+     * 监听队列 - 工作消息队列模式
+     *
+     * @param message
+     */
+    @RabbitListener(queues = WORK_QUEUE_NAME)
+    public void onWorkMessage1(String message) {
+        System.out.println("onWorkMessage1 - 接收到消息：" + message);
+    }
+
+    @RabbitListener(queues = WORK_QUEUE_NAME)
+    public void onWorkMessage2(String message) {
+        System.out.println("onWorkMessage2 - 接收到消息：" + message);
     }
 }
--- durian-rabbitmq/producer-service/src/test/java/ProducerTest.java
+++ durian-rabbitmq/producer-service/src/test/java/ProducerTest.java
@@ -9,12 +9,26 @@ public class ProducerTest {
 
     private static final String QUEUE_NAME = "test-simple-queue";
     private static final String EXCHANGE_NAME = null;
+    private static final String WORK_QUEUE_NAME = "test.work.queue";
 
     @Resource
     private RabbitTemplate rabbitTemplate;
 
+    /**
+     * 简单消息队列模式
+     */
     @Test
     public void testSendMessage() {
         rabbitTemplate.convertAndSend(EXCHANGE_NAME, QUEUE_NAME, "hello world, rabbitmq-4");
+    }
+
+    /**
+     * 工作队列模式
+     */
+    @Test
+    public void testSendWorkMessage() {
+        for (int i = 1; i <= 10; i++) {
+            rabbitTemplate.convertAndSend(EXCHANGE_NAME, WORK_QUEUE_NAME, "hello world, rabbitmq - " + i);
+        }
     }
 }
