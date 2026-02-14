6e6dbf6dff4f9516faec26053e19ec067efca797
zhuangwei <42955983+itzhuangwei@users.noreply.github.com>
2026-02-14 16:10:53 +0800

    feat: 引入feign服务降级

--- durian-system/src/main/java/com/zwx/system/feign/TestFeign.java
+++ durian-system/src/main/java/com/zwx/system/feign/TestFeign.java
@@ -1,12 +1,13 @@
 package com.zwx.system.feign;
 
+import com.zwx.system.feign.impl.TestFeignFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Map;
 
-@FeignClient(name = "durian-demo-serve")
+@FeignClient(name = "durian-demo-serve", fallback = TestFeignFallBack.class)
 public interface TestFeign {
 
     @GetMapping("/durian-demo/demo/info")
--- durian-system/src/main/java/com/zwx/system/feign/impl/TestFeignFallBack.java
+++ durian-system/src/main/java/com/zwx/system/feign/impl/TestFeignFallBack.java
@@ -2,9 +2,11 @@ package com.zwx.system.feign.impl;
 
 import com.zwx.system.feign.TestFeign;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 public class TestFeignFallBack implements TestFeign {
     @Override
     public ResponseEntity<Map<String, String>> getInfo() {
